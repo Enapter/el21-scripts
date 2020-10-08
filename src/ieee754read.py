3611928cd4b6cdf7ad2d14468dbebd30d0089f2b
Alexander Shalin <alexander.shalin@gmail.com>
2020-10-08 16:40:57 +0300

    Update ieee754read.py

--- src/ieee754read.py
+++ src/ieee754read.py
@@ -13,15 +13,15 @@ import struct
 from pymodbus.client.sync import ModbusTcpClient
 
 ip = sys.argv[1]
-port = 502
-#register = 1006 # Stack Total H2 Production (NL)
+PORT = 502
+# register = 1006 # Stack Total H2 Production (NL)
 register = int(sys.argv[2])
 
-device = ModbusTcpClient(ip, port)
-firmware = device.read_input_registers(register, 2, unit=1) # unit here is an address of the slave device
+device = ModbusTcpClient(ip, PORT)
+firmware = device.read_input_registers(register, 2, unit=1)  # unit here is an address of the slave device
 ieee754register = struct.pack("BBBB", firmware.registers[0] >> 8, firmware.registers[0] & 0xff, firmware.registers[1] >> 8, firmware.registers[1] & 0xff)
 ieee754value = struct.unpack(">f", ieee754register)
 
-print('Reading register:',register)
+print('Reading register:', register)
 print('Response (hex):', (hex(firmware.registers[0]) + hex(firmware.registers[1])).replace('0x', ''))
 print('Value: ', str(ieee754value).replace('(', '').replace(')', '').replace(',', ''))
